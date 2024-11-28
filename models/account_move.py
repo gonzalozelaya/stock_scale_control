23df476cb0bbcdb76135bceedf3875a095af5a94
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-11-28 13:17:37 -0300

    Removed unnecesary calc on account.move

--- models/account_move.py
+++ models/account_move.py
@@ -1,6 +1,8 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
+import logging
 
+_logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
@@ -65,43 +67,3 @@ class AccountMove(models.Model):
         return super(AccountMove, self).create(vals)
 
 
-    def _search_default_journal(self):
-        if self.payment_id and self.payment_id.journal_id:
-            return self.payment_id.journal_id
-        if self.statement_line_id and self.statement_line_id.journal_id:
-            return self.statement_line_id.journal_id
-        if self.statement_line_ids.statement_id.journal_id:
-            return self.statement_line_ids.statement_id.journal_id[:1]
-
-        journal_types = self._get_valid_journal_types()
-        company = self.company_id or self.env.company
-        domain = [
-            *self.env['account.journal']._check_company_domain(company),
-            ('type', 'in', journal_types),
-        ]
-
-        journal = None
-        # the currency is not a hard dependence, it triggers via manual add_to_compute
-        # avoid computing the currency before all it's dependences are set (like the journal...)
-        if self.env.cache.contains(self, self._fields['currency_id']):
-            currency_id = self.currency_id.id or self._context.get('default_currency_id')
-            if currency_id and currency_id != company.currency_id.id:
-                currency_domain = domain + [('currency_id', '=', currency_id)]
-                journal = self.env['account.journal'].search(currency_domain, limit=1)
-
-        if not journal:
-            if self.move_type == 'in_invoice':
-                journal = self.env['account.journal'].browse(32)
-            elif self.move_type == 'out_invoice' or self.move_type == 'entry':
-                journal = self.env['account.journal'].browse(21)
-        if not journal:
-            journal = self.env['account.journal'].search(domain, limit=1)
-        if not journal:
-            error_msg = _(
-                "No journal could be found in company %(company_name)s for any of those types: %(journal_types)s",
-                company_name=company.display_name,
-                journal_types=', '.join(journal_types),
-            )
-            raise UserError(error_msg)
-
-        return journal
